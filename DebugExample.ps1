59bb9de69e818a0ca27a7d0de81a0c7fe2c18767
Paul Watts <7057417+PaulFWatts@users.noreply.github.com>
2021-10-27 12:01:49 +1100

    Added vscode config

---
+++ DebugExample.ps1
@@ -0,0 +1,8 @@
+$number = 1
+Write-Host "The number is: " $number
+$number = 2
+Write-Host "The number is: " $number
+$number = 3
+Write-Host "The number is: " $number
+$number = 4
+Write-Host "The number is: " $number
