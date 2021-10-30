7a9102bc77e96c833e0aad8f30a2141f2090f7b7
Paul Watts <7057417+PaulFWatts@users.noreply.github.com>
2021-10-30 16:13:48 +1100

    Fixed [DateTime] variable problem

--- variables.ps1
+++ variables.ps1
@@ -7,7 +7,7 @@
 [double]$var        = 54321.21                                  # similar to decimal but is a 8 byte decimal floating point number.
 [single]$var        = 76549.11                                  # similar to decimal but is a 32 bit floating point number.
 [long]$var          = 2382.22                                   # similar to decimal bus is a 64 bit integer.
-[DateTime]$birthday = "Feburary 12, 1982"                       # Date and time value.
+[DateTime]$birthday = "3/05/2021, 5:00am"                       # Date and time value.
 [array]$var         = "itemone", "itemtwo"                      # Indexed list of objects, in this case an array of string values but they can be any other object type.
 [hashtable]$var     = @{Name = "Ian"; Age = 37; Height = 5.9 }  # Name and Value pairs. You can mix variable types.
 # If you dont specify the variable type PowerShell will determine the best fit.
@@ -16,8 +16,8 @@ $myVariable = "hello this is a string variable"
 [string]$myVariable = "hello this is a string variable"
 # Assigning the number 5 to the int variable $numberVariable
 [int]$numberVariable = 5
-# Assigning $true value to the[…]”
-“Name and Value pairs. You can mix variable types.
+# Assigning $true value to the[…]
+# Name and Value pairs. You can mix variable types.
 # If you dont specify the variable type PowerShell will determine the best fit.
 $myVariable = "hello this is a string variable"
 # It's best practice to always specify the variable type
@@ -37,7 +37,7 @@ $age = 23
 [int]$age = 37
 $age
 # If you want to write text to the console in a flexible way use the Write-Host cmdlet.
-Write-Host "It's my birthday and i'm " $age
+Write-Host "Its my birthday and im " $age
 # Global variables can be used to allow access to them across your script#Player position
 [int]$global:xPosition = 4
 [int]$global:yPosition = 3
